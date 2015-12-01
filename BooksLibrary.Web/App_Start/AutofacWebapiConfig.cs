b373b69444bc7a3ff27e5dd0ef07e405ddadbfbb
vmakhnevich <beatbox89@mail.ru>
2015-12-01 12:08:42 +0300

    Added web project, bootstrapper and bundle config

---
+++ BooksLibrary.Web/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Configuration;
+using System.Data.Entity;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using BooksLibrary.Data;
+using BooksLibrary.Data.Infrastructure;
+using BooksLibrary.Data.Repositories;
+using BooksLibrary.Services;
+using BooksLibrary.Services.Abstract;
+
+namespace BooksLibrary.Web.App_Start
+{
+    public class AutofacWebapiConfig
+    {
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // EF BooksLibraryContext 
+
+            builder.RegisterType<BooksLibraryContext>().As<DbContext>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+
+            builder.RegisterGeneric(typeof(EntityBaseRepository<>)).As(typeof(IEntityBaseRepository<>)).InstancePerRequest();
+
+            // Services 
+            builder.RegisterType<EncryptionService>().As<IEncryptionService>().InstancePerRequest();
+            builder.RegisterType<MembershipService>().As<IMembershipService>().InstancePerRequest();
+            Container = builder.Build();
+            return Container;
+        }
+    }
+}
