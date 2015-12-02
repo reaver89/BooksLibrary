911f71601405b35b879eef76efc736e49a48311a
vmakhnevich <beatbox89@mail.ru>
2015-12-03 01:43:55 +0300

    Message handlers added

--- BooksLibrary.Web/App_Start/WebApiConfig.cs
+++ BooksLibrary.Web/App_Start/WebApiConfig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using BooksLibrary.Web.Infrastucture.MessageHandlers;
 
 namespace BooksLibrary.Web
 {
@@ -10,6 +8,8 @@ namespace BooksLibrary.Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            config.MessageHandlers.Add(new HomeCinemaAuthHandler());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ BooksLibrary.Web/Infrastucture/Extensions/RequestMessageExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Web.Http.Dependencies;
+using BooksLibrary.Services.Abstract;
+
+namespace BooksLibrary.Web.Infrastucture.Extensions
+{
+    public static class RequestMessageExtensions
+    {
+        internal static IMembershipService GetMembershipService(this HttpRequestMessage request)
+        {
+            return request.GetService<IMembershipService>();
+        }
+
+        private static TService GetService<TService>(this HttpRequestMessage request)
+        {
+            IDependencyScope dependencyScope = request.GetDependencyScope();
+            TService service = (TService)dependencyScope.GetService(typeof(TService));
+            return service;
+        }
+    }
+}
